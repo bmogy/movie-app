edb7fde90985d117c175981595f5fd608a384659
bmogy <bmogy@outlook.com>
2020-09-08 00:06:33 -0500

    fifth test

---
+++ build/precache-manifest.1eba6c5be25205b6886de83eb0f1dcff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf1b54cc8fe0a1da4d3c6c9e434089a",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "88af5105a153bb245d24",
+    "url": "/movie-app/static/css/main.6f15a9a1.chunk.css"
+  },
+  {
+    "revision": "5e164f948f27c0a57b8c",
+    "url": "/movie-app/static/js/2.068eeea9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.068eeea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88af5105a153bb245d24",
+    "url": "/movie-app/static/js/main.4965c53b.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  },
+  {
+    "revision": "9b6c257cd113ef676fe9d547b1d56c12",
+    "url": "/movie-app/static/media/movie giffe.9b6c257c.gif"
+  }
+]);
--- src/Layout/Body/TV/TV.js
+++ src/Layout/Body/TV/TV.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import "../../../Styles/Style.css"
-const TV = (props) =>{
+const TV = (props,time) =>{
 return(
     <div>
         <input id="tvInput" onChange={props.inputHandler} placeholder="Please type a name of a TV show"></input>
